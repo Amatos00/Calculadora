dc10339a6fc316d12821c3da8603c83e98781be4
Anderson de Oliveira <amatos.oliveira2197@gmail.com>
2020-08-23 02:15:16 -0300

    first commit

---
+++ arquivo.js
@@ -0,0 +1,36 @@
+function a()
+{ 
+var x=parseInt(document.getElementById("e").value);
+ var y=parseInt(document.getElementById("f").value);
+ var z=x+y; document.getElementById("g").value=z ;
+} 
+//for subtraction
+function b()
+{ 
+var x=parseInt(document.getElementById("e").value);
+ var y=parseInt(document.getElementById("f").value);
+ var z=x-y; 
+  document.getElementById("g").value=z;
+} 
+//for multiplication
+function c()
+{ 
+var x=parseInt(document.getElementById("e").value);
+ var y=parseInt(document.getElementById("f").value);
+ var z=x*y; 
+  document.getElementById("g").value=z ;
+} 
+//for division 
+function d()
+{ 
+var x=parseInt(document.getElementById("e").value);
+ var y=parseInt(document.getElementById("f").value);
+ var z=x/y; 
+  document.getElementById("g").value=z ;
+} 
+//clear button 
+function clearIt() { 
+document.getElementById('e').value = "";
+document.getElementById('f').value = "";
+ document.getElementById('g').value = "";
+}
